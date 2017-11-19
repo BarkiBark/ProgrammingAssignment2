8b64818d2e21dc51c7b6db889c48c79867ce6638
BarkiBark <32178449+BarkiBark@users.noreply.github.com>
2017-11-19 23:57:15 +0200

    Rename cachematrix.R to week3.R

---
+++ week3.R
@@ -0,0 +1,33 @@
+# the first function creates a special “matrix”, which actually is a list containing a function that:
+# set the value of the matrix
+# get the value of the matrix
+# set the value of the inverse
+# get the value of the inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
+
+# second function computes the inverse and cache the result (return a matrix that will be the inverse of 'x')
+cacheSolve <- function(x, ...) {
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
+}
